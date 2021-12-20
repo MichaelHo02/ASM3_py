cb2601d743012221b4ada3a7d39a661147ce8c26
MichaelHo02 <67449167+MichaelHo02@users.noreply.github.com>
2021-12-20 00:44:51 -0500

    update the file

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
-# This is a sample Python script.
+from list_items import list_items
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+ITEMS_FILES = [
+    './data/cloth.csv'
+    './data/drug.csv'
+    './data/food.csv'
+    './data/movie.csv'
+]
+CUSTOMER_FILE = './data/customer.csv'
 
 def menu():
     print(
-        'List all items',
-        'List all info of a specific item',
-        'Search item by name',
-        'Search item by item id',
-        'List all info of a specific customer',
-        'placing orders'
+        'Please press the number to select the option:\n',
+        '[1] List all items\n',
+        '[2] List all info of a specific item\n',
+        '[3] Search item by name\n',
+        '[4] Search item by item id\n',
+        '[5] List all info of a specific customer\n',
+        '[6] Placing orders',
     )
+    user_input = input('Your choice: ')
+    if user_input == '1':
+        print('Process...')
+        list_items()
+    elif user_input == '2':
+        print('Process...')
+    elif user_input == '3':
+        print('Process...')
+    elif user_input == '4':
+        print('Process...')
+    elif user_input == '5':
+        print('Process...')
+    elif user_input == '6':
+        print('Process...')
+    else:
+        pass
+
+
+def import_file():
+    pass
+
+
+if __name__ == '__main__':
+    import_file()
+    menu()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
